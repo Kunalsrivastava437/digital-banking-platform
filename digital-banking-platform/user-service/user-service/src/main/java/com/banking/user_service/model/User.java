11461df0d7bf602c00a00153f3853cf6fedfa7fd
Kunalsrivastava437 <kunalshrivastva437@gmail.com>
2025-08-18 23:48:24 +0530

    first commit

---
+++ digital-banking-platform/user-service/user-service/src/main/java/com/banking/user_service/model/User.java
@@ -0,0 +1,24 @@
+package com.bank.userservice.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    private String role; // e.g. ROLE_ADMIN, ROLE_USER
+}
